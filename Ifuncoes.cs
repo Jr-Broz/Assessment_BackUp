9c06ffd04d3c7adb3166db300a76b27805c402c6
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-09-26 16:10:35 -0300

    Create Ifuncoes.cs

---
+++ Ifuncoes.cs
@@ -0,0 +1,21 @@
+using System;
+namespace Assessment{ 
+public interface IFuncoes{
+
+    //Arquivos Texto
+    void  SalvarEmTexto();
+    void alterarDadosParaTexto();
+    void pesquisarDadoPorNomeParaTexto();
+    void ExcluirDadosParaTexto();
+ 
+
+
+
+//Listas
+ void SalvarEmLista();
+ void AlterarDadosLista();
+ void PesquisarDadoNomeLista();
+void ExcluirDadosParaLista();
+
+    }
+}
